b609b55d9ad28a5f47761fa083c3a266d58eced6
Iman Anwarzai <ianwarzai18@src-code.simons-rock.edu>
2019-03-01 00:02:43 -0500

    done w/ naive

--- points_segments_naive.c
+++ points_segments_naive.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int points_segments_naive(int** lines, int n_lines, int* points, int n_points){
-	int count = 0;
+int I = 2;
+int S = 3;
+void points_segments_naive(int lines[][I], int n_lines, int points[][S], int n_points){
 	for(int i = 0; i < n_points; i++)
 		for(int j = 0; j < n_lines; j++){
-			if(points[i] >= lines[j][0] && points[i] <= lines[j][1])
-					count++;
-		}
-	return count; 
+			if(points[i][0] >= lines[j][0] && points[i][0] <= lines[j][1])
+				points[i][1]++;
+		} 
 }
 
 int main(){
@@ -17,8 +17,9 @@ int main(){
 	//int lines[n_lines][2];
 	//int points[n_points];	
 	
-	int lines[3][2] = {{1, 3}, {2, 5}, {10, 13}};
-	int points[3] = {2, 3, 4};
+	int lines[][2] = {{1, 3}, {2, 5}, {10, 13}};
+	int points[][3] = {{2, 0}, {3, 0}, {4, 0}};
 	
-	printf("%d\n", points_segments_naive(lines, n_lines, points, n_points));
+	points_segments_naive(lines, n_lines, points, n_points);
+	printf("%d %d %d\n", points[0][1], points[1][1], points[2][1]);
 }
